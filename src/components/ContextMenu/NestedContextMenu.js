f7fc2faf9ce820db36bfbe1785c80198caa35f3d
pcpn-bassen <bassen@pcpn.io>
2023-08-03 20:42:30 +0200

    finish submenu

--- src/components/ContextMenu/NestedContextMenu.js
+++ src/components/ContextMenu/NestedContextMenu.js
@@ -206,10 +206,8 @@ const NestedContextMenu = ({
       ref={(ref) => (optionRefs.current[groupIndex] = ref)}
       key={group}
     >
-      <div className="subContextOption">
         <label>{group}</label>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
-      </div>
+        <svg data-flume-component="ctx-submenu-chevron" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 18 18" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
     </SubContextOption>
   )),
   ...Object.entries(groupedOptions)
@@ -293,7 +291,7 @@ const SubContextOption = React.forwardRef(({
 }, ref) => {
   return (
     <div
-      data-flume-component="ctx-menu-option"
+      data-flume-component="ctx-submenu-option"
       className={styles.option}
       role="menuitem"
       onClick={onClick}
